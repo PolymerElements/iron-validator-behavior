dab4b1e32b2062dc076b402e8ffd10d0aa11d500
Elliott Marquez <emarquez@google.com>
2018-06-22 17:04:40 -0700

    auto-converted by polymer-modulizer

--- test/simple-validator.js
+++ test/simple-validator.js
@@ -1,4 +1,4 @@
-<!--
+/**
 @license
 Copyright (c) 2015 The Polymer Project Authors. All rights reserved.
 This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
@@ -6,17 +6,15 @@ The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
 The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
 Code distributed by Google as part of the polymer project is also
 subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
--->
+*/
+import '@polymer/polymer/polymer-legacy.js';
 
-<link rel="import" href="../../polymer/polymer.html">
-<link rel="import" href="../iron-validator-behavior.html">
+import { IronValidatorBehavior } from '../iron-validator-behavior.js';
+import { Polymer } from '@polymer/polymer/lib/legacy/polymer-fn.js';
+Polymer({
 
-<script>
-  Polymer({
+  is: 'simple-validator',
 
-    is: 'simple-validator',
+  behaviors: [IronValidatorBehavior]
 
-    behaviors: [Polymer.IronValidatorBehavior]
-
-  });
-</script>
+});
